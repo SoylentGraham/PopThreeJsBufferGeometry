dc5d1991b173aacf7da5e252990e4d5ff5e75c1e
Graham Reeves <graham@grahamreeves.com>
2016-08-07 00:17:26 +0100

    Initial commit of structure & demo

---
+++ Assets/PopThreeJsBufferGeometry/PopThreeJsBufferGeometry.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class BufferGeometry
+{
+	public string		Name;
+
+	public BufferGeometry(Mesh mesh)
+	{ 
+		Name = mesh.name;
+	}
+};
+
+
+public class PopThreeJsBufferGeometry
+{
+	static public string GetMeshJsonString(Mesh mesh)
+	{
+		var Geo = new BufferGeometry( mesh );
+		return JsonUtility.ToJson( Geo, true );
+	}
+	
+}
