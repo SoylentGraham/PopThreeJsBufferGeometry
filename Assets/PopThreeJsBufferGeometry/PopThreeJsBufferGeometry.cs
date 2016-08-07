d9cf60bfad56277c12204d8b92c93aee978290e6
Graham Reeves <graham@grahamreeves.com>
2016-08-07 18:25:17 +0100

    added mesh data serialisation to json

--- Assets/PopThreeJsBufferGeometry/PopThreeJsBufferGeometry.cs
+++ Assets/PopThreeJsBufferGeometry/PopThreeJsBufferGeometry.cs
@@ -18,18 +18,17 @@ public class BufferGeometry4_Meta
 [Serializable]
 public class BufferGeometry4_Attributes
 {
-	public float[]	position;
-	public float[]	normal;
-	public float[]	uv;
-	public float[]	uv2;
-	public float[]	color;
- 
+	public float[]	position;	//	3x
+	public float[]	normal;		//	3x
+	public float[]	uv;			//	2x
+	public float[]	uv2;		//	2x
+	public float[]	color;		//	3x
 };
 
 [Serializable]
 public class BufferGeometry4_Data
 {
-	public float[]						index;
+	public int[]						index;
 	public BufferGeometry4_Attributes	attributes;
 };
 
@@ -73,11 +72,82 @@ public class BufferGeometry4
 	public BufferGeometry4_Meta		metadata;
 	public BufferGeometry4_Data		data;
 
+	void WriteFloatArray(ref float[] FloatArray, Vector3[] VectorArray)
+	{ 
+		if ( VectorArray == null )
+		{
+			FloatArray = null;
+			return;
+		}
+
+		int VectorSize = 3;
+		//	todo: expand array for merging multiple meshes
+		FloatArray = new float[VectorArray.Length * VectorSize];
+		for ( int i=0;	i<VectorArray.Length;	i++ )
+		{ 
+			var Pos3 = VectorArray[i];
+			FloatArray[(i*VectorSize)+0] = Pos3.x;
+			FloatArray[(i*VectorSize)+1] = Pos3.y;
+			FloatArray[(i*VectorSize)+2] = Pos3.z;
+		}
+	}
+
+	void WriteFloatArray(ref float[] FloatArray, Vector2[] VectorArray)
+	{ 
+		if ( VectorArray == null )
+		{
+			FloatArray = null;
+			return;
+		}
+	
+		int VectorSize = 2;
+		//	todo: expand array for merging multiple meshes
+		FloatArray = new float[VectorArray.Length * VectorSize];
+		for ( int i=0;	i<VectorArray.Length;	i++ )
+		{ 
+			var Pos2 = VectorArray[i];
+			FloatArray[(i*VectorSize)+0] = Pos2.x;
+			FloatArray[(i*VectorSize)+1] = Pos2.y;
+		}
+	}
+
+	//	for this json it's RGB, not RGBA
+	void WriteFloatArray(ref float[] FloatArray, Color[] VectorArray)
+	{ 
+		if ( VectorArray == null )
+		{
+			FloatArray = null;
+			return;
+		}
+	
+		int VectorSize = 3;
+		//	todo: expand array for merging multiple meshes
+		FloatArray = new float[VectorArray.Length * VectorSize];
+		for ( int i=0;	i<VectorArray.Length;	i++ )
+		{ 
+			var Pos2 = VectorArray[i];
+			FloatArray[(i*VectorSize)+0] = Pos2.r;
+			FloatArray[(i*VectorSize)+1] = Pos2.g;
+			FloatArray[(i*VectorSize)+2] = Pos2.b;
+		}
+	}
+
 	public BufferGeometry4(Mesh mesh,int Version=0)
 	{ 
 		metadata = new BufferGeometry4_Meta();
 		metadata.version = Version;
 		data = new BufferGeometry4_Data();
+		data.attributes = new BufferGeometry4_Attributes();
+
+		//	serialise data
+		WriteFloatArray( ref data.attributes.position, mesh.vertices );
+		WriteFloatArray( ref data.attributes.normal, mesh.normals );
+		WriteFloatArray( ref data.attributes.uv, mesh.uv );
+		WriteFloatArray( ref data.attributes.uv2, mesh.uv2 );
+		WriteFloatArray( ref data.attributes.color, mesh.colors );
+
+		//	write triangle indexes - currently just a direct copy
+		data.index = mesh.triangles;
 	}
 };
 
