bf71dc98b7218605f66d2a142ac42c373820dffd
Graham Reeves <graham@grahamreeves.com>
2016-08-07 20:29:06 +0100

    Using correct attribute layouts. (Can't serialise templates!)

--- Assets/PopThreeJsBufferGeometry/PopThreeJsBufferGeometry.cs
+++ Assets/PopThreeJsBufferGeometry/PopThreeJsBufferGeometry.cs
@@ -15,22 +15,69 @@ public class BufferGeometry4_Meta
 };
 
 
+
+[Serializable]
+public class BufferGeometry4_Attribute_Float
+{
+	public BufferGeometry4_Attribute_Float(int VectorSize)
+	{ 
+		itemSize = VectorSize;
+	}
+
+	public float[]	array;
+	public string	type = "Float32Array";
+	public bool		normalized = false;
+	public int		itemSize = 3;
+};
+
+
+[Serializable]
+public class BufferGeometry4_Attribute_Int
+{
+	public BufferGeometry4_Attribute_Int(int VectorSize=1)
+	{ 
+		itemSize = VectorSize;
+	}
+
+	public int[]	array;
+	public string	type = "Int32Array";
+	public bool		normalized = false;
+	public int		itemSize = 1;
+};
+
+
 [Serializable]
 public class BufferGeometry4_Attributes
 {
-	public float[]	position;	//	3x
-	public float[]	normal;		//	3x
-	public float[]	uv;			//	2x
-	public float[]	uv2;		//	2x
-	public float[]	color;		//	3x
+	public BufferGeometry4_Attribute_Float	position;	//	3x
+	public BufferGeometry4_Attribute_Float	normal;		//	3x
+	public BufferGeometry4_Attribute_Float	uv;			//	2x
+	public BufferGeometry4_Attribute_Float	uv2;		//	2x
+	public BufferGeometry4_Attribute_Float	color;		//	3x
 };
+
 
 [Serializable]
 public class BufferGeometry4_Data
 {
-	public int[]						index;
-	public BufferGeometry4_Attributes	attributes;
+	public BufferGeometry4_Attribute_Int	indicies;
+	public BufferGeometry4_Attributes		attributes;
 };
+
+
+/*
+var TYPED_ARRAYS = {
+			'Int8Array': Int8Array,
+			'Uint8Array': Uint8Array,
+			'Uint8ClampedArray': Uint8ClampedArray,
+			'Int16Array': Int16Array,
+			'Uint16Array': Uint16Array,
+			'Int32Array': Int32Array,
+			'Uint32Array': Uint32Array,
+			'Float32Array': Float32Array,
+			'Float64Array': Float64Array
+		};
+*/
 
 
 //	https://github.com/mrdoob/three.js/blob/d8b24a1d2d4887a5db77d7112e5ba1539377c905/src/core/Geometry.js#L224   
@@ -72,17 +119,18 @@ public class BufferGeometry4
 	public BufferGeometry4_Meta		metadata;
 	public BufferGeometry4_Data		data;
 
-	void WriteFloatArray(ref float[] FloatArray, Vector3[] VectorArray)
+	void WriteAttribute(ref BufferGeometry4_Attribute_Float Attribute, Vector3[] VectorArray)
 	{ 
 		if ( VectorArray == null )
 		{
-			FloatArray = null;
+			Attribute = null;
 			return;
 		}
 
 		int VectorSize = 3;
 		//	todo: expand array for merging multiple meshes
-		FloatArray = new float[VectorArray.Length * VectorSize];
+		Attribute = new BufferGeometry4_Attribute_Float(VectorSize);
+		float[] FloatArray = new float[VectorArray.Length * VectorSize];
 		for ( int i=0;	i<VectorArray.Length;	i++ )
 		{ 
 			var Pos3 = VectorArray[i];
@@ -90,64 +138,73 @@ public class BufferGeometry4
 			FloatArray[(i*VectorSize)+1] = Pos3.y;
 			FloatArray[(i*VectorSize)+2] = Pos3.z;
 		}
+		Attribute.array = FloatArray;
 	}
 
-	void WriteFloatArray(ref float[] FloatArray, Vector2[] VectorArray)
+	void WriteAttribute(ref BufferGeometry4_Attribute_Float Attribute, Vector2[] VectorArray)
 	{ 
 		if ( VectorArray == null )
 		{
-			FloatArray = null;
+			Attribute = null;
 			return;
 		}
-	
+
 		int VectorSize = 2;
 		//	todo: expand array for merging multiple meshes
-		FloatArray = new float[VectorArray.Length * VectorSize];
+		Attribute = new BufferGeometry4_Attribute_Float(VectorSize);
+		float[] FloatArray = new float[VectorArray.Length * VectorSize];
 		for ( int i=0;	i<VectorArray.Length;	i++ )
 		{ 
-			var Pos2 = VectorArray[i];
-			FloatArray[(i*VectorSize)+0] = Pos2.x;
-			FloatArray[(i*VectorSize)+1] = Pos2.y;
+			var Pos3 = VectorArray[i];
+			FloatArray[(i*VectorSize)+0] = Pos3.x;
+			FloatArray[(i*VectorSize)+1] = Pos3.y;
 		}
+		Attribute.array = FloatArray;
 	}
 
+
 	//	for this json it's RGB, not RGBA
-	void WriteFloatArray(ref float[] FloatArray, Color[] VectorArray)
+	void WriteAttribute(ref BufferGeometry4_Attribute_Float Attribute, Color[] VectorArray)
 	{ 
 		if ( VectorArray == null )
 		{
-			FloatArray = null;
+			Attribute = null;
 			return;
 		}
-	
+
 		int VectorSize = 3;
 		//	todo: expand array for merging multiple meshes
-		FloatArray = new float[VectorArray.Length * VectorSize];
+		Attribute = new BufferGeometry4_Attribute_Float(VectorSize);
+		float[] FloatArray = new float[VectorArray.Length * VectorSize];
 		for ( int i=0;	i<VectorArray.Length;	i++ )
 		{ 
-			var Pos2 = VectorArray[i];
-			FloatArray[(i*VectorSize)+0] = Pos2.r;
-			FloatArray[(i*VectorSize)+1] = Pos2.g;
-			FloatArray[(i*VectorSize)+2] = Pos2.b;
+			var Pos3 = VectorArray[i];
+			FloatArray[(i*VectorSize)+0] = Pos3.r;
+			FloatArray[(i*VectorSize)+1] = Pos3.g;
+			FloatArray[(i*VectorSize)+2] = Pos3.b;
 		}
+		Attribute.array = FloatArray;
 	}
+
 
 	public BufferGeometry4(Mesh mesh,int Version=0)
 	{ 
 		metadata = new BufferGeometry4_Meta();
 		metadata.version = Version;
 		data = new BufferGeometry4_Data();
-		data.attributes = new BufferGeometry4_Attributes();
 
+		data.attributes = new BufferGeometry4_Attributes();
+	
 		//	serialise data
-		WriteFloatArray( ref data.attributes.position, mesh.vertices );
-		WriteFloatArray( ref data.attributes.normal, mesh.normals );
-		WriteFloatArray( ref data.attributes.uv, mesh.uv );
-		WriteFloatArray( ref data.attributes.uv2, mesh.uv2 );
-		WriteFloatArray( ref data.attributes.color, mesh.colors );
+		WriteAttribute( ref data.attributes.position, mesh.vertices );
+		WriteAttribute( ref data.attributes.normal, mesh.normals );
+		WriteAttribute( ref data.attributes.uv, mesh.uv );
+		WriteAttribute( ref data.attributes.uv2, mesh.uv2 );
+		WriteAttribute( ref data.attributes.color, mesh.colors );
 
 		//	write triangle indexes - currently just a direct copy
-		data.index = mesh.triangles;
+		data.indicies = new BufferGeometry4_Attribute_Int();
+		data.indicies.array = mesh.triangles;
 	}
 };
 
